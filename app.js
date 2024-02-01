40de0b7f769be5481fe64f3b7734b2ad29ccfcfe
amit1208 <amitrajput.chauhan@gmail.com>
2024-02-01 11:34:19 +0530

    ! removed

---
+++ app.js
@@ -0,0 +1,22 @@
+const express =require('express')
+// import express from "express"
+const app=express()
+const port=3000;
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world!')
+})
+
+app.get('/lorem',(req,res)=>{
+    res.send('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat')
+})
+
+
+app.listen(port,()=>{
+    console.log(`Listening on Port ${port}`)
+})
+
+
+// console.log("chai aur code");
+// console.log("testing")
